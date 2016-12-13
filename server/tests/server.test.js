14bce95ec3a55eb96b288ff2cd213dc116b8d751
Santhosh Racherla <Santhosh Racherla>
2016-12-12 20:13:51 -0600

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,39 @@ describe('GET /todos/:id', (done) => {
   });
 
 });
+
+
+describe('DELETE /todos/:id', (done) => {
+  it('should remove a todo', (done) => {
+    request(app)
+      .delete(`/todos/${todos[1]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[1].text);
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+        Todo.findById(todos[1]._id.toHexString()).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    request(app)
+        .delete(`/todos/new ObjectID().toHexString()`)
+        .expect(404)
+        .end(done);
+  });
+
+
+  it('should return a 404 if object id is invalid', (done) => {
+    request(app)
+    .delete(`/todos/1234`)
+    .expect(404)
+    .end(done);
+  });
+});
