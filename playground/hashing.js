a99805dfad54f4b3c9efa93337db894252d8ee34
Santhosh Racherla <Santhosh Racherla>
2016-12-18 12:15:30 -0600

    Hasg User password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 const {SHA256} =  require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
 
-var data = {
-  id: 10
-};
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err,hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var token = jwt.sign(data, '123abc')
-console.log(token);
+var hashedPassword = '$2a$10$0H5YLeyEgrhP9mjOVRdpOODrBfiGqhUML6I1ebp7BUkxSLBGh19jS';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
-var decoded =jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+//
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc')
+// console.log(token);
+//
+// var decoded =jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 //
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
